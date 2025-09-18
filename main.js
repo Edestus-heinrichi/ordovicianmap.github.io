8ab27846576ba3f6f854fee54458bd1b1ef513fc
Edestus-heinrichi <shreyas11j@gmail.com>
2025-09-18 09:12:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 time() {
-  var time = 486 - int(document.getElementById('timeInput').innerText)
+  var time = 486 - int(document.getElementById('timeInput').innerHTML)
   document.getElementById('timeOutput').innerText = time
 }
